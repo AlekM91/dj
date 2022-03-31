8bf95134c578335e54b10c40b6a2493eb5d42d0a
Aleksandar Mitrevski <aleksandarmitrevski91@gmail.com>
2022-03-31 16:48:44 +0200

    dj site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,37 @@ if(module.hot) {
     module.hot.accept();
 }
 
-let hamburger = document.querySelector('.hamburger');
-let mainNav = document.querySelector('.main-nav');
-let body = document.querySelector('body');
+function menuFunctionality() {
+    const hamburger = document.querySelector('.hamburger');
+    const mainNav = document.querySelector('.main-nav');
+    const nav = document.querySelectorAll('.nav');
+    const body = document.querySelector('body');
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    mainNav.classList.toggle('main-nav-active')
-    body.classList.toggle('js-no-scroll')
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('hamburger-active');
+        mainNav.classList.toggle('main-nav-active')
+        body.classList.toggle('js-no-scroll')
+    })
 
-})
+    function closeMenu() {
+        hamburger.classList.remove('hamburger-active');
+        mainNav.classList.remove('main-nav-active')
+        body.classList.remove('js-no-scroll')
+    }
+
+    nav.forEach(n => {
+        n.addEventListener('click', function(e) {
+            e.preventDefault();
+            if (e.target.classList.contains('nav-link')) {
+                closeMenu();
+            }
+
+            const id = e.target.getAttribute('href');
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+    })
+}
+
+menuFunctionality();
